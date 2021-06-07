0ae03c5e3a1d1f932a4497fcd9f0da4f9e8b209d
Anne-Sophie Cusson <64850625+4nn3-50ph13@users.noreply.github.com>
2021-06-06 22:19:23 -0400

    slideshow

--- script.js
+++ script.js
@@ -445,17 +445,57 @@ if (chosenFiltres != null) {
 }
 
 var resetBtn = document.querySelector('.soumettre.reset');
-
-resetBtn.addEventListener('click', function() {
-    localStorage.removeItem('chosenFiltres');
-    for (const element of chkbx){
-        check(element.value,false);
-    }
-});
+if (resetBtn){
+    resetBtn.addEventListener('click', function() {
+        localStorage.removeItem('chosenFiltres');
+        for (const element of chkbx){
+            check(element.value,false);
+        }
+    });
+}
 
 function check(value,checked) {
     const cbs = document.querySelectorAll('input[value='+value+']');
-    cbs.forEach((cb) => {
-        cb.checked = checked;
-    });
+    console.log("cbs")
+    console.log(cbs)
+    if (cbs != null){
+        cbs.forEach((cb) => {
+            cb.checked = checked;
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
